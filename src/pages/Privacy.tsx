c591c1f38ef6eeb9dc3024ad5c8472282cfface9
BANER <vova.shamin.0503@gmail.com>
2025-03-03 10:25:07 +0200

    Update Privacy.tsx

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -1,6 +1,6 @@
 
 import { motion } from "framer-motion";
-import { Navbar } from "@/components/Navbar";
+import { Navbar } from "../components/Navbar.tsx"
 import { Footer } from "@/components/Footer";
 
 const Privacy = () => {
